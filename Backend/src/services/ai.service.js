6641f06de93cd3b10828fec252ab6b53acf36db0
lovish714 <lovishtalwar07@gmail.com>
2026-09-04 23:05:19 +0530

    fix: update main entry point and enhance Puppeteer launch options for PDF generation

--- Backend/src/services/ai.service.js
+++ Backend/src/services/ai.service.js
@@ -59,8 +59,15 @@ async function generateInterviewReport({ resume, selfDescription, jobDescription
 
 
 
-async function generatePdfFromHtml(htmlContent) {
-    const browser = await puppeteer.launch()
+async function generatePdfFromHtml(htmlContent) {   
+    const browser = await puppeteer.launch({
+         headless: true,
+    args: [
+        "--no-sandbox",
+        "--disable-setuid-sandbox",
+        "--disable-dev-shm-usage"
+    ]
+    })
     const page = await browser.newPage();
     await page.setContent(htmlContent, { waitUntil: "networkidle0" })
 
